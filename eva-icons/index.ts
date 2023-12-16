3a8426e76714198c4f62478d13e42b6edd0f296b
David Lee <me@tica.fun>
2023-12-16 16:58:19 +0900

    ➕ Added export of search icon

--- eva-icons/index.ts
+++ eva-icons/index.ts
@@ -1,3 +1,4 @@
 export { Brush } from "./icons/brush.tsx";
 export { Compass } from "./icons/compass.tsx";
+export { Search } from "./icons/search.tsx";
 export type { EvaIcon, EvaProps } from "./types.d.ts";
